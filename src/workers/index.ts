d58e796f44671e3ee6e9bbe096ebb4fff9da3e4a
Yuichiro MASUI <masui@masuidrive.jp>
2022-10-12 16:27:45 +0000

    型チェックを調整中

--- src/workers/index.ts
+++ src/workers/index.ts
@@ -1,34 +1,70 @@
 import { COLOR_INDEX_LABEL, TRAIN_DATA, TEST_DATA } from "../dataset";
 
-// 1次元配列の中身を全部足す
+/// 配列の次元数を求める
+function array_dims(x: any): number {
+    if (Array.isArray(x)) {
+        return array_dims(x[0]) + 1;
+    }
+    return 0;
+}
+
+/// 0で埋めた1次元配列を返す
+function fill_zero_1d(size: number) {
+    return [...Array(size)].map(() => 0);
+}
+
+/// 0から(maxNum ?? size)までの重複しないランダムな値の入った、要素数sizeの1次元配列を返す
+function non_overlapping_random_1d(size: number, maxNum?: number): number[] {
+    const nums = [...Array(maxNum ?? size)].map((_, i) => i);
+    return nums.sort((a, b) => 0.5 - Math.random()).slice(0, size);
+}
+
+// 0から1.0のランダムな値で埋められた二次元配列を返す
+function random_2d(height: number, width: number): number[][] {
+    return [...Array(height)].map((v) =>
+        [...Array(width)].map((v) => Math.random())
+    );
+}
+
+// 二次元配列の各要素にvalを掛ける
+function product_2d_number(x: number[][], val: number) {
+    return x.map((xx) => xx.map(xxx => xxx * val));
+}
+
+/// 1次元配列の中身を全部足す
 function sum_1d(x: number[]): number {
     return x.reduce((v, s) => v + s, 0);
 }
 
-// 1次元配列の中にマイナスの数字があれば0にする
+/// 1次元配列の中にマイナスの数字があれば0にする
 function relu_1d(x: number[]): number[] {
     return x.map((v) => Math.max(v, 0));
 }
 
-// 配列の中の値の上下関係を保ったまま、合計値が1.0に調整する
+/// 配列の中の値の上下関係を保ったまま、合計値が1.0に調整する
 function softmax_1d(x: number[]): number[] {
     const base = sum_1d(x.map((v) => Math.exp(v)));
     return x.map((v) => Math.exp(v) / base);
 }
 
-// 1次元配列と2次元配列の内積(ドット積)を求める
+/// 1次元配列の一番大きな値のインデックス値
+function argmax_1d(vector: number[]) {
+    return vector.map((val, i) => [val, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
+}
+
+/// 1次元配列と2次元配列の内積(ドット積)を求める
 function dot_produce_1d_2d(x: number[], y: number[][]): number[] {
     return [...Array(y[0].length)].map((_, w1i) =>
         sum_1d(x.map((xx, i) => xx * y[i][w1i]))
     )
 }
 
-// 同じサイズの1次元配列同士の足し算
+/// 同じサイズの1次元配列同士の足し算
 function add_1d(x: number[], y: number[]): number[] {
     return x.map((xx, i) => xx + y[i]);
 }
 
-// weightsとxから推測の1次元配列を返す
+/// weightsとxから推測の1次元配列を返す
 function predict(x: number[], weights: any): number[] {
     // x.shape,W1.shape,b1.shape,a1.shape,
     // shape  x(3), W1(3, 30), b1(30,), a1(100, 30)
@@ -41,7 +77,7 @@ function predict(x: number[], weights: any): number[] {
     return y;
 }
 
-// 1次元の配列の傾きを求める
+/// 1次元の配列の傾きを求める
 function numerical_gradient_1d(loss_func: any, x: number[]) {
     const h = 0.0001;
     let grad = [...Array(x.length)].map((v) => 0);
@@ -60,14 +96,14 @@ function numerical_gradient_1d(loss_func: any, x: number[]) {
     return grad;
 }
 
-// 2次元の配列の傾きを求める
+/// 2次元の配列の傾きを求める
 function numerical_gradient_2d(loss_func: any, x: number[][]) {
     return x.map((val, idx) =>
         numerical_gradient_1d(loss_func, val)
     );
 }
 
-// クロスエントロピーロスを計算
+/// クロスエントロピーロスを計算
 function cross_entropy_error(y: number[][], t: number[]) {
     const h = 0.0000001;
     const batch_size = y.length;
@@ -78,15 +114,10 @@ function cross_entropy_error(y: number[][], t: number[]) {
     return val;
 }
 
-// ロス関数
+/// ロス関数
 function loss(x: number[][], t: number[], weights: any) {
     const y = x.map((xx) => predict(xx, weights))
     return cross_entropy_error(y, t);
-}
-
-// 1次元配列の一番大きな値のインデックス値
-function argmax_1d(vector: number[]) {
-    return vector.map((val, i) => [val, i]).reduce((r, a) => (a[0] > r[0] ? a : r))[1];
 }
 
 // 正解率を求める
@@ -96,49 +127,32 @@ function accuracy(x: number[][], t: number[], weights: any) {
 }
 
 // x:入力データ, t:教師データ
-function numerical_gradient(x: number[][], t: number[], weights: any) {
+function numerical_gradient(x: number[][], t: number[], weights: any): { [key: string]: number[][] | number[] } {
     let grads: { [key: string]: number[][] | number[] } = {};
-    ["W1", "b1", "W2", "b2"].forEach((key) => {
-        if (Array.isArray(weights[key][0])) {
-            (grads as any)[key] = numerical_gradient_2d(
-                (_: number) => loss(x, t, weights),
+    for (const key in weights) {
+        if (array_dims(weights[key]) === 2) {
+            grads[key] = numerical_gradient_2d(
+                (w: number) => loss(x, t, weights),
                 weights[key]
             );
-        } else {
-            (grads as any)[key] = numerical_gradient_1d(
-                (_: number) => loss(x, t, weights),
+        } else if (array_dims(weights[key]) === 1) {
+            grads[key] = numerical_gradient_1d(
+                (w: number) => loss(x, t, weights),
                 weights[key]
             );
         }
-    });
+    };
 
     return grads;
 }
 
-function init_weight(
-    weight_init_std: number,
-    input_size: number,
-    output_size: number
-) {
-    return [...Array(input_size)].map((v) =>
-        [...Array(output_size)].map((v) => Math.random() * weight_init_std)
-    );
-}
-
-// 0で埋めた1次元配列を返す
-function fill_zero_1d(size: number) {
-    return [...Array(size)].map(() => 0);
-}
-
-// 0からmaxNum ?? sizeまでの重複しないランダムな値の入った、要素数sizeの1次元配列を返す
-function random_1d(size: number, maxNum?: number): number[] {
-    const nums = [...Array(maxNum ?? size)].map((_, i) => i);
-    return nums.sort((a, b) => 0.5 - Math.random()).slice(0, size);
-}
+const is1D = <T>(maybeArray: T | readonly T[]): maybeArray is T[] => {
+    return Array.isArray(maybeArray);
+};
 
 export function hello() {
     const iters_num = 1000;
-    const hidden_size = 30;
+    const hidden_size = 4;
 
     const train_size = TRAIN_DATA.length;
     const input_size = TRAIN_DATA[0].length - 1;
@@ -148,10 +162,10 @@ export function hello() {
     const learning_rate = 0.001;
 
     const weight_init_std = 0.01;
-    let weights = {
-        W1: init_weight(weight_init_std, input_size, hidden_size),
+    let weights: { [key: string]: number[][] | number[] } = {
+        W1: product_2d_number(random_2d(input_size, hidden_size), weight_init_std),
         b1: fill_zero_1d(hidden_size),
-        W2: init_weight(weight_init_std, hidden_size, output_size),
+        W2: product_2d_number(random_2d(hidden_size, output_size), weight_init_std),
         b2: fill_zero_1d(output_size)
     };
     //console.log("init weights", JSON.stringify(weights));
@@ -160,7 +174,7 @@ export function hello() {
     for (let iter = 0; iter < iters_num; ++iter) {
         // ミニバッチの取得
         // ランダムにbatch_size個分のtrainRGB, trainColorIndexを取り出す
-        const batch_mask = random_1d(batch_size, train_size);
+        const batch_mask = non_overlapping_random_1d(batch_size, train_size);
         const trainRGB = batch_mask.map((i) => TRAIN_DATA[i].slice(1, 4));
         const trainColorLabel = batch_mask.map((i) => TRAIN_DATA[i][0]);
 
@@ -168,18 +182,17 @@ export function hello() {
         const grad = numerical_gradient(trainRGB, trainColorLabel, weights);
 
         // パラメータの更新
-        ["W1", "b1", "W2", "b2"].forEach((key) => {
-            (weights as any)[key] = (weights as any)[key].map((row: any, rowi: number) => {
-                if (row[0] === undefined) { // 1D
-                    return row - learning_rate * (grad as any)[key][rowi];
+        for (const key in weights) {
+            weights[key] = weights[key].map((row: number | number[], rowi: number) => {
+                if (is1D(row)) {
+                    return row.map((val: number, vali: number) =>
+                        val - learning_rate * ((grad as any)[key][rowi][vali] as number));
                 }
                 else {
-                    return row.map((val: number, vali: number) =>
-                        val - learning_rate * (grad as any)[key][rowi][vali]);
+                    return row - learning_rate * (grad[key][rowi] as number);
                 }
-            })
-            //console.log("key",key, matrix_shape( (weights as any)[key] ),matrix_shape( (grad as any)[key] ) )
-        });
+            }) as number[] | number[][]
+        };
         if (iter % iter_per_epoch == 0) {
             const train_acc = accuracy(TRAIN_DATA.map((data) => data.slice(1, 4)), TRAIN_DATA.map((data) => data[0]), weights)
             const test_acc = accuracy(TEST_DATA.map((data) => data.slice(1, 4)), TEST_DATA.map((data) => data[0]), weights)
